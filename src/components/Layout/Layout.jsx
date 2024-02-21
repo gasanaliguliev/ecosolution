1155b0fac7e8c12aa290685ef334728d0e26967c
gasanaliguliev <aligulievgasan001@gmail.com>
2024-02-21 17:40:18 -0500

    fix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
-import {Footer} from "../Footer/Footer";
-import {Header} from "../Header/Header";
-import {Main} from "../Main/Main";
+import { Footer } from "components/Footer/Footer"
+import { Header } from "components/Header/Header"
+import { Main } from "components/Main/Main"
+
 
 export const Layout = () => {
     return(
