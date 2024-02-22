15c679ca0109679c4252cf78d8f6e91135562e38
gasanaliguliev <aligulievgasan001@gmail.com>
2024-02-21 21:46:13 -0500

    last fix pushed main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import { MainContainer } from './Main.styled';
+import { Hero } from '../Hero/Hero';
+
+
+export const Main = () => {
+  return (
+    <MainContainer>
+      <Hero />
+    </MainContainer>
+  );
+};
+
